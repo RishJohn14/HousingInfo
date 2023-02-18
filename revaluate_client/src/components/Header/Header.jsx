1b079e623e1d6c2db0b9da34f57c7696eacca05d
alexusljf <limjinfeng@live.com>
2023-02-18 17:33:41 +0800

    made changes

--- revaluate_client/src/components/Header/Header.jsx
+++ revaluate_client/src/components/Header/Header.jsx
@@ -4,10 +4,10 @@ import './Header.css';
 function Header(){
     return(
         <div>
-            <nav class = "headerBar">
-            <a href = '/' class = 'revaluateLink'> REvaluate </a>
-            <a href = '/about' class = 'aboutLink'> About Us </a>
-            <a href = '/insights' class = 'insightsLink'> Get Insights </a>
+            <nav className = "headerBar">
+            <a href = '/' className = 'revaluateLink'> REvaluate </a>
+            <a href = '/about' className = 'aboutLink'> About Us </a>
+            <a href = '/insights' className = 'insightsLink'> Get Insights </a>
             </nav>
         </div>
     )
