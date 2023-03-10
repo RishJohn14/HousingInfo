519a9e35ec840bc3789dbc4cef12040c5ab8fb8d
alexusljf <limjinfeng@live.com>
2023-03-10 21:57:51 +0800

    Update PriceFilterBar.jsx

--- revaluate_client/src/components/PriceFilterBar/PriceFilterBar.jsx
+++ revaluate_client/src/components/PriceFilterBar/PriceFilterBar.jsx
@@ -4,51 +4,57 @@ import './PriceFilterBar.css';
 function PriceFilterBar(){
     const [minPrice, setMinPrice] = useState(0);
     const [maxPrice, setMaxPrice] = useState(10000000);
-   
-    const handleChangeMin = (event) => {
+    
+    const handleChange = (event, type) => {
         const value = event.target.value;
         if(isNaN(value) == true ){
             alert("Invalid, Please enter Numeric values only.");
+            event.target.value = '';
             return false;
         } 
         if(value < 0){
             alert("Invalid, Price entered is negative.");
+            event.target.value = '';
             return false;
         }
         if(value > 10000000){
             alert("Invalid, Price entered exceeds the maximum value.");
+            event.target.value = '';
             return false;
         }
-        setMinPrice(value);
+        if (type == 'MIN'){
+            /* doesn't function properly because setState is 1 state behind
+            if (value > maxPrice){
+                alert("Invalid range.");
+                event.target.value = '';
+                return false;
+            }
+            */
+            setMinPrice(value); 
+        }
+        else{
+            /* doesn't function properly because setState is 1 state behind
+            if (value < minPrice){
+                alert("Invalid range.");
+                event.target.value = '';
+                return false;
+            }
+            */
+            setMaxPrice(value); 
+        }
         console.log(value);
+        console.log("min value = " + minPrice);
+        console.log("max value = " + maxPrice);
     }
-    const handleChangeMax = (event) => {
-        const value = event.target.value;
-        if(isNaN(value) == true ){
-            alert("Invalid, Please enter Numeric values only.");
-            return false;
-        } 
-        if(value < 0){
-            alert("Invalid, Price entered is negative.");
-            return false;
-        }
-        if(value > 10000000){
-            alert("Invalid, Price entered exceeds the maximum value.");
-            return false;
-        }
-        setMaxPrice(value);
-        console.log(value);
-    }
-      return(
+    
+    return(
         <div className = 'filterBar'>
             <p>
                 Only show transactions between
             </p>
-            <input id = "minInputBox" type = 'text' placeholder = 'Minimum value' onChange={handleChangeMin} value={minPrice} ></input>
-            <p>
-                to
-            </p>
-            <input id = "maxInputBox" type = 'text' placeholder = 'Maximum value' onChange={handleChangeMax} value={maxPrice}></input>
+            <input id = "minInputBox" type = 'text' placeholder = 'Minimum value' onChange={(e) => handleChange(e, 'MIN')} ></input>
+            <p>to</p>
+            <input id = "maxInputBox" type = 'text' placeholder = 'Maximum value' onChange={(e) => handleChange(e, 'MAX')} ></input>
         </div>
     )
 }
