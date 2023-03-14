13473a4a9c0737a14d8491523190f7b566310e21
auglxw <augustineleexuanwei@gmail.com>
2023-03-15 00:22:05 +0800

    styling for card

--- revaluate_client/src/components/Map/Map.jsx
+++ revaluate_client/src/components/Map/Map.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import "./Map.css"
-import { GoogleMap, useLoadScript, MarkerF, InfoWindowF } from '@react-google-maps/api';
+import { GoogleMap, useLoadScript, MarkerF } from '@react-google-maps/api';
 import CloseIcon from '@mui/icons-material/Close';
+import { Grid } from '@mui/material';
+import Carousel from 'react-bootstrap/Carousel';
 
 function Map(){
     const { isLoaded } = useLoadScript({googleMapsApiKey: process.env.REACT_APP_GMAP_API_KEY});
@@ -10,6 +12,7 @@ function Map(){
     const [town, updateTown] = useState('Neighbourhood');
     const [selectedPin, updateSelectedPin] = useState();
     const [cardVisible, updateCardVisibility] = useState(false);
+    const [activeCarouselIndex, updateActiveCarouselIndex] = useState(0);
 
     function handleMarkerPress(idx) {
         updateSelectedPin(idx);
@@ -64,9 +67,46 @@ function Map(){
                     position={{lat: data.lat, lng: data.lng}}
                 />)}
                 <div className={cardVisible ? "cardVisible" : "cardNotVisible"}>
-                    <div className="closeContainer">
-                        <CloseIcon className="closeButton" onClick={() => updateCardVisibility(false)} />
-                    </div>
+                    <Grid container>
+                        <Grid item xs={11}>
+                            <p className="swipe">Click next for more transactions in the same block {'>>'}</p>
+                        </Grid>
+                        <Grid item xs={1}>
+                            <CloseIcon className="closeButton" onClick={() => updateCardVisibility(false)} />
+                        </Grid>
+                    </Grid>
+                    <Carousel
+                        activeIndex={activeCarouselIndex}
+                        onSelect={(idx) => {updateActiveCarouselIndex(idx)}}
+                        variant="dark"
+                        interval={null}
+                        className="carousell"
+                    >
+                        <Carousel.Item>
+                            <p className="roadName">Holland Close</p>
+                            <p className="block">Block 123</p>
+                            <p className="flatType">5-Room Flat</p>
+                            <p className="sellingPriceLabel">Selling Price</p>
+                            <p className="sellingPrice">$850,000</p>
+                            <p className="transactionDetailsLabel">Transaction Details</p>
+                            <p className="transactionDetails">Flat Size</p>
+                            <p className="transactionDetails">Tenure Left</p>
+                            <p className="transactionDetails">Proximity to MRT</p>
+                            <p className="transactionDetails">Year Sold</p>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <p className="roadName">Holland Close</p>
+                            <p className="block">Block 123</p>
+                            <p className="flatType">5-Room Flat</p>
+                            <p className="sellingPriceLabel">Selling Price</p>
+                            <p className="sellingPrice">$850,000</p>
+                            <p className="transactionDetailsLabel">Transaction Details</p>
+                            <p className="transactionDetails">Flat Size</p>
+                            <p className="transactionDetails">Tenure Left</p>
+                            <p className="transactionDetails">Proximity to MRT</p>
+                            <p className="transactionDetails">Year Sold</p>
+                        </Carousel.Item>
+                    </Carousel>
                 </div>
             </GoogleMap> }
         </div>
