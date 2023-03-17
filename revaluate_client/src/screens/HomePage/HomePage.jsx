1ab108d9201f4e8f4ab67d730a0e9ba933965c01
auglxw <augustineleexuanwei@gmail.com>
2023-03-17 18:17:16 +0800

    switch button setup

--- revaluate_client/src/screens/HomePage/HomePage.jsx
+++ revaluate_client/src/screens/HomePage/HomePage.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './HomePage.css'
 import { Grid } from '@mui/material';
 import Header from '../../components/Header/Header';
+import SwitchButton from '../../components/SwitchButton/SwitchButton';
 
 function HomePage() {
     return (<Grid container>
         <Grid item xs={7}>
             <Header/>
             <p>REvaluate</p>
+            <SwitchButton />
         </Grid>
         <Grid item xs={5}>
             <img src={require('./houseVectorImg.png')} alt={'house img'} className='homeImg' />
