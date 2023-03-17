58b83a108f8509cc2871b47dd9ab2115de4f5ad8
Lavanya <lavanyas32002@gmail.com>
2023-03-17 18:07:28 +0800

    DetailsPage static UI

---
+++ revaluate_client/src/components/Navigation Bar/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TransparentBar = () => {
+  return (
+    <nav className="transparent-bar">
+      {/* Your navigation content goes here */}
+    </nav>
+  );
+};
+
+export default TransparentBar;
