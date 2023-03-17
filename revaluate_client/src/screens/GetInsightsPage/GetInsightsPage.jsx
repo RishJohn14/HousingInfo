7d111bd2e8603401ff95f607acbafbe96a3026c9
Tan Jia Ze <jz@Tans-MBP.mshome.net>
2023-03-17 18:41:42 +0800

    test

--- revaluate_client/src/screens/GetInsightsPage/GetInsightsPage.jsx
+++ revaluate_client/src/screens/GetInsightsPage/GetInsightsPage.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
+import './GetInsightsPage.css'
 
 function GetInsightsPage() {
     return (
         <div>
         <Header/>
-        <p>Insights page</p>
+            <div className="big-container">
+            <div className="grid-container container-insights">
+                <div className="grid-item">1</div>
+                <div className="grid-item">2</div>
+                <div className="grid-item">3</div>
+                <div className="grid-item">4</div>
+            </div>
+            </div>
+
+
         </div>
     )
 }
